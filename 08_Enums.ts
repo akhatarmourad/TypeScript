a97e22ea797e72b4520d1a9ded8f0b18167a679b
akhatarmourad <mourad.akhatar01@gmail.com>
2024-11-10 18:04:03 +0100

    Enums is TS

---
+++ 08_Enums.ts
@@ -0,0 +1,17 @@
+/* Enumerations in TypeScript */
+enum CourseChoices {
+    NEXTJS,
+    REACTJS,
+    REDUX,
+    NESTJS,
+    JEST
+}
+
+const myChoice = CourseChoices.REDUX;
+
+enum Softwares {
+    POWERBI = 10,
+    TABLEAU,
+    EXCEL = "MicroSoft Excel",
+    OFFICE = 45,
+}
