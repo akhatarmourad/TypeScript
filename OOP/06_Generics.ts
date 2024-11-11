3af63c8ad1e7be4cec62298f15b3de62723e9d02
akhatarmourad <mourad.akhatar01@gmail.com>
2024-11-11 12:13:16 +0100

    Generics with constraints

--- OOP/06_Generics.ts
+++ OOP/06_Generics.ts
@@ -13,3 +13,25 @@ function getLength<T>(scores: T[]): T {
 
 // * Arrow functions
 const getLen = <T>(scores: T[]): T => scores[scores.length - 1];
+
+// * Generic Types with interfaces & constraints
+interface Database {
+    connection: string;
+    username: string;
+    password: string;
+}
+
+
+function getConnection<T, U extends Database>(t: T, db: U): object {
+    return {};
+}
+
+interface Cloud {
+    name: string,
+    provider: string,
+    certificates: Array<string>
+}
+
+class AWS<T> {
+    public services: T[] = [];
+}
