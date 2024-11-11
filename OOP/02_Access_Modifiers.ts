b43f37c7cff1d1f7cdcf42ff731dde8b2a963057
akhatarmourad <mourad.akhatar01@gmail.com>
2024-11-11 10:34:20 +0100

    Access Modifiers

--- OOP/02_Access_Modifiers.ts
+++ OOP/02_Access_Modifiers.ts
@@ -3,5 +3,12 @@ class Course {
     public duration: number;
     private isCompleted: boolean;
     protected secretKey: string;
-     
+}
+
+/* a brief way to create attributes & constructor */
+class Project {
+    constructor(public title: string, private budget: number) {
+        this.title = title;
+        this.budget = budget;
+    }
 }
