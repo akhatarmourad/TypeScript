7e60d5fd0fa6ff81a2e941f6221637d681abd1fa
akhatarmourad <mourad.akhatar01@gmail.com>
2024-11-11 11:50:05 +0100

    Generics with Arrow functions

--- OOP/06_Generics.ts
+++ OOP/06_Generics.ts
@@ -1,6 +1,15 @@
+// * Generic types with variables
 const scores: Array<number> = [];
 const subjects: Array<string> = [];
 
+// * Generic types with functions
 function getResult<T>(id: T): T {
     return id;
 }
+
+function getLength<T>(scores: T[]): T {
+    return scores[scores.length - 1];
+}
+
+// * Arrow functions
+const getLen = <T>(scores: T[]): T => scores[scores.length - 1];
